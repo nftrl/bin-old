ad4b4b9380ce674864a1762f30935b927a19f1fb
Marcus Larsen (Akoya) <marcuslarsen97@gmail.com>
2017-11-02 15:25:09 +0100

    Improve by a mile
    logwan.sh now takes an obligatory argument INTERFACE, that specifies the interface
    used to get the LAN ip address.
    
    By default logwan.sh now prints to stdout, but it takes an optional
    argument LOGFILE - if given, logwan.sh instead appends to the specified
    file.
    
    The script now has 4 exit statuses: 0 on success; 1 if no arguments were
    given (in this case it prints the help message); 2 if an error occured
    when finding LAN ip address; 3 if an error occured when finding WAN ip
    address.
    
    Add help message.

--- logwan.sh
+++ logwan.sh
@@ -1,24 +1,40 @@
 #!/bin/bash
-# Log WAN and LAN ip addresses to file ip.log
 
-# Create log file ip.log if it doesn't exist.
-if [ ! -f 'ip.log' ]
+function printhelp {
+	echo Usage: ./logwan.sh [--help] INTERFACE [LOGFILE]
+	echo 
+	echo Log WAN and LAN ip addresses. By default, print to stdout. If LOGFILE
+	echo is given, instead append output to LOGFILE and do not print to stdout.
+	echo Create LOGFILE if it doesn\'t exist.
+}
+
+# Read arguments
+if [ "$#" -eq 0 ]
 then
-	touch ip.log
+	printhelp
+	exit 1
+elif [[ "$1" == '--help' ]]
+then
+	printhelp
+	exit 0
 fi
+INTERFACE="$1"
+LOGFILE="$2"
 
+# Get date and ip addresses. If an error happens, print error message and exit
+# with return value.
 DATE="$(date)"
-LAN="$(ifconfig wlp2s0 | nl | grep -E " 2" | grep -oE "(addr:.* B)" | grep -oE "([0-9]\.?)*")"
-WAN="$(dig +short myip.opendns.com @resolver1.opendns.com)"
-WANRETVAL="$?"
+LAN="$(ifconfig "$INTERFACE" | nl | grep -E " 2" | grep -oE "(addr:.* B)" | grep -oE "([0-9]\.?)*")" || exit 2
+WAN="$(dig +short myip.opendns.com @resolver1.opendns.com)" || exit 3
 
-# If WANRETVAL != 0 it probably means that there is no internet connection.
-# Abandon ship with exit status WANRETVAL.
-if [ "$WANRETVAL" -ne 0 ]
+# Print output to stdout or LOGFILE
+if [ -z "$LOGFILE" ]
 then
-	exit "$WANRETVAL"
+	printf "DATE\t%b\nWAN \t%b\nLAN \t%b\n\n" "$DATE" "$WAN" "$LAN"
+else
+	# Create log file if it doesn't exist.
+	if [ ! -f "$LOGFILE" ]; then touch "$LOGFILE"; fi
+	printf "DATE\t%b\nWAN \t%b\nLAN \t%b\n\n" "$DATE" "$WAN" "$LAN" >> "$LOGFILE"
 fi
 
-# If all is good, append to log file.
-printf "DATE\t%b\nWAN \t%b\nLAN \t%b\n\n" "$DATE" "$WAN" "$LAN" >> ip.log
 exit 0
